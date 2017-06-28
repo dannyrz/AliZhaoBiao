73fa1f277680d73a48df6a8383ba2706bfe9f9f1
zhurizhao <zhurizhao@hotmail.com>
2017-06-28 16:04:19 +0800

    deal hidden fileds in form.

--- Project/views.py
+++ Project/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # 引入我们创建的表单类
 from Project.form import CreateForm
+from Project.models import Info
 import time;
 
 # Create your views here.
 
 def create(request):
     if request.method == 'POST':  # 当提交表单时
-        form = CreateForm(request.POST)  # form 包含提交的数据
+        info = Info(IID=time.time())
+        form = CreateForm(request.POST, instance=info)
+        form.save()
 
         if form.is_valid():  # 如果提交的数据合法
             form.save()
             return HttpResponse("Create Success.")
 
     else:  # 当正常访问时
-        form = CreateForm(initial={'IID': time.time()})
+        form = CreateForm()
     return render(request, 'Project/create.html', {'form': form})
