75827d0ae8bc01cfd38c942cc772c0b85ae36aa9
zhurizhao <zhurizhao@hotmail.com>
2017-07-14 09:05:42 +0800

    query task from mysqldb

--- Chong/program.py
+++ Chong/program.py
@@ -31,26 +31,33 @@ def scanTask():
 		sql = "SELECT * FROM chong_task"
 		cursor.execute(sql)
 		task={}
-		for row in cursor.fetchall():
-			task["IID"]=row[1];
-			task["Name"]=row[2];
-			task["Charset"]=row[3];
-			task["InURLRegularExpression"]=row[4];
-			task["InURLNumber"]=row[5];
-			task["PageProperty"]=row[6];
-			task["PagePropertyRegularExpression"]=row[7];
-			task["BodyFilterTextRegularExpression"]=row[8];
-			task["TextReplace"]=row[9];
-			task["WorkInterval"]=row[10];
-			task["ThreadNumber"]=row[11];
-			task["DatabaseConnectStr"]=row[12];
-			task["Status"]=row[13];
-			task["EnableBroswer"]=row[14];
-			task["ResponseFormat"]=row[15];
-			task["Account"]=row[16];
-			task["NeedLogin"]=row[17];
-			task["SpiderName"]=row[18];
-			dispatchTaskThread(task)
+		index = cursor.description
+		result = []
+		for res in cursor.fetchall():
+			row = {}
+			for i in range(len(index)-1):
+				row[index[i][0]] = res[i]
+			result.append(row)
+		# for row in cursor.fetchall():
+		# 	task["IID"]=row[1];
+		# 	task["Name"]=row[2];
+		# 	task["Charset"]=row[3];
+		# 	task["InURLRegularExpression"]=row[4];
+		# 	task["InURLNumber"]=row[5];
+		# 	task["PageProperty"]=row[6];
+		# 	task["PagePropertyRegularExpression"]=row[7];
+		# 	task["BodyFilterTextRegularExpression"]=row[8];
+		# 	task["TextReplace"]=row[9];
+		# 	task["WorkInterval"]=row[10];
+		# 	task["ThreadNumber"]=row[11];
+		# 	task["DatabaseConnectStr"]=row[12];
+		# 	task["Status"]=row[13];
+		# 	task["EnableBroswer"]=row[14];
+		# 	task["ResponseFormat"]=row[15];
+		# 	task["Account"]=row[16];
+		# 	task["NeedLogin"]=row[17];
+		# 	task["SpiderName"]=row[18];
+		dispatchTaskThread(result)
 		cursor.close()
 		connect.close()
 		time.sleep(60)
